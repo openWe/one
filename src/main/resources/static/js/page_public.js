a8ef8a7deef50f13e9becad47af0efa47a56bba3
zhanghongyu <zhang_hongyuzhang_hongyu@venusgroup.com.cn>
2020-06-22 11:22:02 +0800

    添加全屏功能

--- src/main/resources/static/js/page_public.js
+++ src/main/resources/static/js/page_public.js
@@ -316,13 +316,87 @@ $(function () {
             "name": "通报次数"
         }]
     });
-
-
 })
 
+//表格数据填充
+function initDataTable(){
+    let data = {};
+    $('#mytab').bootstrapTable({
+        url: "",//数据源
+        dataField: "rows",//服务端返回数据键值 就是说记录放的键值是rows，分页时使用总记录数的键值为total
+        height: function tableHeight() {
+            return $(window).height() - 50;
+        },//高度调整
+        search: true,//是否搜索
+        pagination: true,//是否分页
+        pageSize: 20,//单页记录数
+        pageList: [5, 10, 20, 50],//分页步进值
+        sidePagination: "server",//服务端分页
+        contentType: "application/x-www-form-urlencoded",//请求数据内容格式 默认是 application/json 自己根据格式自行服务端处理
+        dataType: "json",//期待返回数据类型
+        method: "post",//请求方式
+        searchAlign: "left",//查询框对齐方式
+        queryParamsType: "limit",//查询参数组织方式
+        queryParams: function getParams(params) {
+            //params obj
+            params.other = "otherInfo";
+            return params;
+        },
+        searchOnEnterKey: false,//回车搜索
+        showRefresh: true,//刷新按钮
+        showColumns: true,//列选择按钮
+        buttonsAlign: "left",//按钮对齐方式
+        toolbar: "#toolbar",//指定工具栏
+        toolbarAlign: "right",//工具栏对齐方式
+        columns: [
+            {
+                title: "全选",
+                field: "select",
+                checkbox: true,
+                width: 20,//宽度
+                align: "center",//水平
+                valign: "middle"//垂直
+            },
+            {
+                title: "ID",//标题
+                field: "id",//键名
+                sortable: true,//是否可排序
+                order: "desc"//默认排序方式
+            },
+            {
+                field: "name",
+                title: "NAME",
+                sortable: true,
+                titleTooltip: "this is name"
+            },
+            {
+                field: "age",
+                title: "AGE",
+                sortable: true,
+            },
+            {
+                field: "info",
+                title: "INFO[using-formatter]",
+                formatter: 'infoFormatter',//对本列数据做格式化
+            }
+        ],
+        onClickRow: function(row, $element) {
+            //$element是当前tr的jquery对象
+            $element.css("background-color", "green");
+        },//单击row事件
+        locale: "zh-CN", //中文支持
+        detailView: false, //是否显示详情折叠
+        detailFormatter: function(index, row, element) {
+            var html = '';
+            $.each(row, function(key, val){
+                html += "<p>" + key + ":" + val +  "</p>"
+            });
+            return html;
+        }
+    });
+}
 
 //攻击地图
-
 function loadAttckMap() {
 
 
